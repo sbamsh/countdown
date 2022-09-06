584cd39b123580269e1e90a49f3fc53ec53ff844
Saba Mirshahi <sabamirri@gmail.com>
2022-09-06 16:01:01 +0430

    codes for flip cards added

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
 //SELECTORS
 
+const flipCard = document.querySelector(".flip-card");
 
-
-
-
-
-
-//LISTENERS
-
-
-
-
+flip(flipCard)
 
 
 
 
 //FUNCTIONS
+
+function flip(flipCard) {
+    const topHalf = flipCard.querySelector(".top");
+    const bottomHalf = flipCard.querySelector(".bottom");
+    const topFlip = document.createElement("div");
+    topFlip.classList.add("top-flip")
+    const bottomFlip = document.createElement("div");
+    bottomFlip.classList.add("bottom-flip")
+    const startNumber = parseInt(topHalf.textContent)
+    
+    top.textContent = startNumber;
+    bottomHalf.textContent = startNumber;
+    topFlip.textContent = startNumber;
+    bottomFlip.textContent = startNumber - 1;
+
+//listeners
+    
+topFlip.addEventListener("animationstart", e => {
+    topHalf.textContent =startNumber - 1;
+})
+
+topFlip.addEventListener("animationend", e => {
+    topFlip.remove();
+})
+
+bottomFlip.addEventListener("animationstart", e => {
+    bottomHalf.textContent =startNumber - 1;
+    bottomFlip.remove()
+})
+flipCard.append(topFlip, bottomFlip);
+
+}
